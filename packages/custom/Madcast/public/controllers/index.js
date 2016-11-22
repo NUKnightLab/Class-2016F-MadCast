95cadf3a69fe1e6b9fa8d6af1fd126af3b44603c
David Latimore II <dgl@u.northwestern.edu>
2016-11-22 14:59:54 -0600

    set up user data collection in angular

--- packages/custom/Madcast/public/controllers/index.js
+++ packages/custom/Madcast/public/controllers/index.js
@@ -10,7 +10,40 @@ angular.module(('mean.system', ['myApp.filters', 'myApp.services', 'myApp.direct
 
   angular.module('mean.system', ['ngSanitize'])
 .controller('ExampleController', ['$scope', function($scope) {
-  $scope.myHTML = 
+  $scope.myHTML =
      'I am an <code>HTML</code>string with ' +
      '<a href="#">links!</a> and other <em>stuff</em>';
+}]);
+
+
+angular.module('mean.system').controller('Controlla', ['$scope', function($scope) {
+  $scope.master = {};
+  $scope.hero = {
+    name: 'Spawn'
+  };
+
+  $scope.update = function(user) {
+       $scope.master = angular.copy(user);
+       if ($scope.user.feeling == "cheerful") {
+          console.log("You are a Hang Out Homie");
+       };
+       if ($scope.user.feeling == "funny") {
+          console.log("You are Jovial Jokester");
+       };
+       if ($scope.user.feeling == "informative") {
+          console.log("You're a Curious Cat!");
+       };
+       if ($scope.user.feeling == "somber") {
+          console.log("You are a Mopey Muppet");
+       };
+       if ($scope.user.feeling == "serious") {
+          console.log("You are a News Nerd!");
+       };
+     };
+
+     $scope.reset = function() {
+       $scope.user = angular.copy($scope.master);
+     };
+
+     $scope.reset();
 }]);
